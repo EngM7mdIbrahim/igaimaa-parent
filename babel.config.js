9fb014f83364ddfc29c4900339279d09a4374103
Mohamed Ibrahim Fathy <eng.mohammedibrahim904@gmail.com>
2022-07-26 05:55:06 +0200

    Created First Project Sturcture

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            _assets: "./src/assets",
+            _components: "./src/components",
+            _atoms: "./src/components/atoms",
+            _molecules: "./src/components/molecules",
+            _organisms: "./src/components/organisms",
+            _navigations: "./src/navigations",
+            _scenes: "./src/scenes",
+            _api: "./src/api",
+            _styles: "./src/styles",
+            _utils: "./src/utils",
+          },
+        },
+      ],
+    ],
   };
 };
